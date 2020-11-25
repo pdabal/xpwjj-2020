e67d85c21e490a2a7d732025a8c0362f5b52d0bc
Paweł Dąbal <pawel.dabal@wat.edu.pl>
2020-11-25 14:52:07 +0100

    Dodanie aplikacji Maven z GUI

--- Lecture/V/DemoMaven/src/main/java/pl/edu/wat/wel/ztc/App.java
+++ Lecture/V/DemoMaven/src/main/java/pl/edu/wat/wel/ztc/App.java
@@ -1,5 +1,6 @@
 package pl.edu.wat.wel.ztc;
 import com.fazecast.jSerialComm.*;
+import java.awt.EventQueue;
 /**
  * Hello world!
  *
@@ -12,5 +13,26 @@ public class App
         for (SerialPort sp : SerialPort.getCommPorts()) {
             System.out.println(sp.getSystemPortName());
         }
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        EventQueue.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        });
     }
 }
